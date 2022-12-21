7433b7a0df31d7ce2e91e67b0a930978818d3479
K33P <ronaldo.fabbi@k33p.com.br>
2022-12-21 00:21:06 -0300

    Adicionar arquivos de projeto.

---
+++ PN_Syngenta/Form1.b1f.cs
@@ -0,0 +1,44 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace PN_Syngenta
+{
+    [FormAttribute("PN_Syngenta.Form1", "Form1.b1f")]
+    class Form1 : UserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.ComboBox0 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_0").Specific));
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_2").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.ComboBox ComboBox0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+        private SAPbouiCOM.Button Button0;
+    }
+}
